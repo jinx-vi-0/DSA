3c65a29ee19dfdd874947f540929546b0f3903fe
Adarsh Kumar <120160355+jinx-vi-0@users.noreply.github.com>
2023-09-22 21:43:17 +0530

    Update BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -8,18 +8,47 @@ public class BinarySearch {
         }
         System.out.print("enter element(1-50): ");
         int key = in.nextInt();
+
+        int ans = binary_search(arr, key);
+        if(ans == -1)
+            System.out.println("element not found");
+        else
+            System.out.println("element found at index :" + ans);
+    }
+    public static int binary_search(int[] arr, int target) {
         int start=0, end=arr.length-1;
         while(start<=end) {
             int mid = (start+end)/2;
             if(key==arr[mid]) {
-                System.out.println("element found at index " + mid);
-                return;
+                return mid;
             }
             if(key>arr[mid])
                 start=mid+1;
             else
                 end=mid-1;
         }
-        System.out.println("element not found");
+        return -1;
+    }
+    public static int first_Occurence(int[] arr, int key) {
+        int st = 0, end = arr.length-1;
+        while(st < end) {
+            int mid = (st+end)/2;
+            if(arr[mid] < key)
+                st = mid + 1;
+            else
+                end = mid;
+        }
+        return (arr[st] == key) ? st : -1;
+    }
+    public static int last_Occurence(int[] arr, int key) {
+        int st = 0, end = arr.length-1;
+        while(st < end) {
+            int mid = (st+end+1)/2;
+            if(arr[mid] <= key)
+                st = mid;
+            else
+                end = mid - 1;
+        }
+        return (arr[st] == key) ? st : -1;
     }
 }
